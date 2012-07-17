354fe8ba6bfde601b74a853cdf08da8dfd5f6de1
techo <techo@Ubuntu>
2012-07-16 23:19:00 -0400

    test

---
+++ phpmyadmin/config-db.php
@@ -0,0 +1,19 @@
+<?php
+##
+## database access settings in php format
+## automatically generated from /etc/dbconfig-common/phpmyadmin.conf
+## by /usr/sbin/dbconfig-generate-include
+## Sat, 14 Jul 2012 17:30:33 -0400
+##
+## by default this file is managed via ucf, so you shouldn't have to
+## worry about manual changes being silently discarded.  *however*,
+## you'll probably also want to edit the configuration file mentioned
+## above too.
+##
+$dbuser='phpmyadmin';
+$dbpass='2012sclt';
+$basepath='';
+$dbname='phpmyadmin';
+$dbserver='';
+$dbport='';
+$dbtype='mysql';
